4bcebdcefa05cda8716db7e5988ea8ff4e12d55d
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2024-05-13 16:54:29 +0100

    feat: Using React's state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
+import OBR, { Item, Metadata } from '@owlbear-rodeo/sdk';
 import React, { useState } from 'react';
 
 import Container from 'react-bootstrap/Container';
+import { setupContextMenu } from './contextMenu';
+import { ID } from './main';
 
-interface AppProps {
-  locationKeys: any[];
-}
+interface LocationKey { description: string; name: string; }
 
-const App: React.FC<AppProps> = ({locationKeys}: AppProps) => {
-  const [sLocationKeys] = useState(locationKeys);
+const App: React.FC = () => {
+  const [locationKeys, setLocationKeys] = useState<LocationKey[]>([]);
+
+  const getItemText = (item: any) => {
+    return item.text.richText[0].children[0].text;
+  }
+  
+  const handleOnChange = (items: Item[]): void => {
+    const newLocationKeys: LocationKey[] = [];
+    
+    // Loads the existing location keys
+    for (const item of items) {
+      if (item.metadata[`${ID}/metadata`]) {
+        const metadata = item.metadata[`${ID}/metadata`] as Metadata;
+        newLocationKeys.push({
+          description: metadata.locationKey as string,
+          name: getItemText(item),
+        });
+      }
+    }
+
+    // Sorts the location keys by name
+    newLocationKeys.sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+    });
+
+    setLocationKeys(newLocationKeys);
+  }
+
+  OBR.onReady(() => {
+    setupContextMenu();
+    OBR.scene.items.onChange(handleOnChange);
+  });
   
   return (
     <Container className="p-3">
@@ -17,11 +51,11 @@ const App: React.FC<AppProps> = ({locationKeys}: AppProps) => {
         </h1>
       </Container>
       <ul id='location-keys'>
-        {sLocationKeys.map((locationKey, index) => (
+        {locationKeys.map((locationKey, index) => (
           <li key={index}>
             {locationKey.name}
             <br />
-            {locationKey.locationKey}
+            {locationKey.description}
           </li>
         ))}
       </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,11 @@
-import OBR from "@owlbear-rodeo/sdk";
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import { setupContextMenu } from "./contextMenu";
-import { setupInitiativeList } from "./initiativeList";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-import { createRoot } from 'react-dom/client';
 
 export const ID = "es.memorablenaton.map-location-keys";
 const container = document.getElementById('app');
 const root = createRoot(container!);
 
-root.render(<App locationKeys={[]} />);
-
-OBR.onReady(() => {
-  setupContextMenu();
-  setupInitiativeList(document.querySelector("#location-keys"));
-});
+root.render(<App />);

00869a0c4a64f0df7ff3f1b96d0e1a797ea214cf
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2024-05-23 08:44:46 +0100

    Updated help

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -2,32 +2,61 @@ import React from "react";
 import { Accordion, Card, CardBody, Container } from "react-bootstrap";
 
 const Help: React.FC = () => {
-  const items: { header: string; image: string }[] = [
-    { header: "1. Add or select an existing TEXT item", image: "img/help01.png" },
-    { header: "2. Click the \"Add Location Key\" button", image: "img/help02.png" },
+  const itemsAdd: { header: string; image: string }[] = [
+    {
+      header: "1. Add or select an existing TEXT item",
+      image: "img/help01.png",
+    },
+    {
+      header: '2. Click the "Add Location Key" button',
+      image: "img/help02.png",
+    },
     { header: "3. Edit Location Key's description", image: "img/help03.png" },
-    { header: "4. Save your changes", image: "img/help04.png" }
+    { header: "4. Save your changes", image: "img/help04.png" },
+  ];
+  const itemsExport: { header: string; text: string }[] = [
+    {
+      header: "1. Click the Export button",
+      text: "A YAML file will be downloaded to your computer.",
+    },
   ];
   return (
-    <Container className="p-3">
-      <Card className="mb-4">
-        <CardBody>
-          <Card.Title>
-            How to Add Location Keys
-          </Card.Title>
-        </CardBody>
-      </Card>
-      <Accordion key="1">
-        {items.map((item, index) => (
-          <Accordion.Item eventKey={String(index)} key={index}>
-            <Accordion.Header>{item.header}</Accordion.Header>
-            <Accordion.Body>
-              <img src={item.image} alt={item.header} className="img-fluid" />
-            </Accordion.Body>
-          </Accordion.Item>
-        ))}
-      </Accordion>
-    </Container>
+    <>
+      <Container className="p-3">
+        <Card className="mb-2">
+          <CardBody>
+            <Card.Title>Adding Location Keys</Card.Title>
+          </CardBody>
+        </Card>
+        <Accordion key="1">
+          {itemsAdd.map((item, index) => (
+            <Accordion.Item eventKey={String(index)} key={index}>
+              <Accordion.Header>{item.header}</Accordion.Header>
+              <Accordion.Body>
+                <img src={item.image} alt={item.header} className="img-fluid" />
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      </Container>
+      <Container className="p-3">
+        <Card className="mb-2">
+          <CardBody>
+            <Card.Title>Exporting Location Keys</Card.Title>
+          </CardBody>
+        </Card>
+        <Accordion key="1">
+          {itemsExport.map((item, index) => (
+            <Accordion.Item eventKey={String(index)} key={index}>
+              <Accordion.Header>{item.header}</Accordion.Header>
+              <Accordion.Body>
+                { item.text }
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      </Container>
+    </>
   );
 };
 

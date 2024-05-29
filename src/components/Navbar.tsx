923534e0fe12b4bf39e5bba0aec11411f8dbaf08
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2024-05-29 14:47:30 +0100

    New Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  Nav,
+  NavDropdown,
+  Navbar as RBNavbar,
+  Row,
+} from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
 
 import { isDevMode } from "../utils";
@@ -11,20 +18,55 @@ const Navbar: React.FC = () => {
       {isDevMode() ? (
         <div className="alert alert-warning p-3">Development Mode</div>
       ) : null}
-      <nav className="navbar p-3">
-        <Link className="nav-item nav-link" to="/">
-          Key List
-        </Link>
-        <Link className="nav-item nav-link" to={paths.bulkActions}>
-          Bulk Actions
-        </Link>
-        <Link className="nav-item nav-link" to={paths.importExport}>
-          Import/Export
-        </Link>
-        <Link className="nav-item nav-link" to={paths.help}>
-          Help
-        </Link>
-      </nav>
+      <RBNavbar>
+        <Container>
+          <Row>
+            <Col xs={8} className="text-right">
+              <RBNavbar.Brand>
+                <Link className="nav-item nav-link" to="/">
+                  Location Keys
+                </Link>
+              </RBNavbar.Brand>
+            </Col>
+            <Col xs={4}>
+              <RBNavbar.Toggle aria-controls="basic-navbar-nav" />
+              <RBNavbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto navbar-right">
+                  <NavDropdown title="Menu" id="basic-nav-dropdown">
+                    <NavDropdown.Item>
+                      <Link className="nav-item nav-link" to="/">
+                        Home
+                      </Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>
+                      <Link
+                        className="nav-item nav-link"
+                        to={paths.bulkActions}
+                      >
+                        Bulk Actions
+                      </Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>
+                      <Link
+                        className="nav-item nav-link"
+                        to={paths.importExport}
+                      >
+                        Import/Export
+                      </Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                      <Link className="nav-item nav-link" to={paths.help}>
+                        Help
+                      </Link>
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </RBNavbar.Collapse>
+            </Col>
+          </Row>
+        </Container>
+      </RBNavbar>
       <Outlet />
     </Container>
   );

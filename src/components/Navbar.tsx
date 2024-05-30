3dd2bc246265de33df83589ea9828c0ed1e5a00a
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2024-05-30 10:26:03 +0100

    Renaming

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar: React.FC = () => {
               <NavDropdown title="Menu" id="basic-nav-dropdown">
                 <NavDropdown.Item>
                   <Link className="nav-item nav-link" to="/">
-                    Existing Location Keys
+                    Location Keys
                   </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item>

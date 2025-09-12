ad30ce7ecb1816b04ecb342ab824426549895e86
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2025-09-12 19:41:15 +0100

    Refactor LocationKeys component to consolidate the "Buy Me A Coffee" section for improved readability.

--- src/components/LocationKeys.tsx
+++ src/components/LocationKeys.tsx
@@ -170,17 +170,17 @@ const LocationKeys: React.FC<{
           </CardBody>
         </Card>
       )}
+      <Container className="p-3 text-center">
+        <a href="https://www.buymeacoffee.com/alvarocavalcanti" target="_blank">
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="Buy Me A Coffee"
+            style={{ height: "60px", width: "217px" }}
+          />
+        </a>
+      </Container>
     </Container>
   );
-  <Container className="p-3 text-center">
-    <a href="https://www.buymeacoffee.com/alvarocavalcanti" target="_blank">
-      <img
-        src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-        alt="Buy Me A Coffee"
-        style={{ height: "60px", width: "217px" }}
-      />
-    </a>
-  </Container>;
 };
 
 export default LocationKeys;
